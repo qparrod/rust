6a9b507f1d344fb17260c2b0cb8f4598ddae816c
qparrod <qparrod@gmail.com>
2016-02-27 14:06:56 +0100

    test threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
+use std::thread;
+
+static NTHREADS: i32 = 10;
+
 fn main()
 {
-    let x = 13;
-    let y = test(6);
-    println!("hello world {}", y);
+    println!("test result {}", test(6));
+    
+    let mut children = vec![];
+    let mut done     = false;
+
+    for i in 0..NTHREADS
+    {
+        // create thread
+        let child = thread::spawn(move ||
+        {
+            let mut biere = 0;
+            println!("this is thread number {}", i);
+            while !done
+            {
+                if biere == 100000 { done = true }
+                biere += 1
+            }
+            println!("thread {} : cave remplie", i);
+        });
+        // Spin up another thread
+        children.push(child);
+    }
+
+    for child in children
+    {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
+    }
 }
 
 fn test(x: i32) -> i32 {
